9e1f71aacf05051062f08770dcbd80a82972b95d
yangoff <yangovip@gmail.com>
2021-01-07 10:12:30 -0300

    Config Umbler

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-const url = 'mongodb://localhost:27017/todo'
+// const url = 'mongodb://localhost:27017/todo'
+const url = 'mongo_todoappdb:27017/todo'
 const usr = 'todoadmin'
 const pass = 'todoadmintestdb'
 const source = 'admin'
