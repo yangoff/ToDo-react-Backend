1a34480db8de8e5b35887fa2fe875205b7213be9
yangoff <yangovip@gmail.com>
2021-01-07 08:55:23 -0300

    Add auth

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 const url = 'mongodb://localhost:27017/todo'
+const usr = 'todoadmin'
+const pass = 'todoadmintestdb'
+const source = 'admin'
 
-mongoose.connect(url, { useNewUrlParser: true});
+mongoose.connect(url, { useNewUrlParser: true,authSource:source,user:usr,pass:pass});
+
 
 module.exports = mongoose;
