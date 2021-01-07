9e3d10d4daef7073debe996fba29467184fba294
yangoff <yangovip@gmail.com>
2021-01-07 10:27:47 -0300

    Umbler config

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,9 @@ const usr = 'todoadmin'
 const pass = 'todoadmintestdb'
 const source = 'admin'
 
-mongoose.connect(url, { useNewUrlParser: true,authSource:source,user:usr,pass:pass});
+// mongoose.connect(url, { useNewUrlParser: true,authSource:source,user:usr,pass:pass});
 
-
+mongoose.connect(url, {authSource:source,user:usr,pass:pass,useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log('MongoDB Connected...'))
+    .catch((err) => console.log(err))
 module.exports = mongoose;
