ec2fd4127e5271ce0797918029cd3ad92fb63b08
yangoff <yangovip@gmail.com>
2021-01-07 10:42:58 -0300

    Umbler Config

--- src/config/database.js
+++ src/config/database.js
@@ -3,11 +3,11 @@ const mongoose = require('mongoose')
 const url = 'mongo_todoappdb:27017/todoappdb'
 const usr = 'todoadmin'
 const pass = 'todoadmintestdb'
-const source = 'admin'
+const source = 'todoappdb'
 
 // mongoose.connect(url, { useNewUrlParser: true,authSource:source,user:usr,pass:pass});
 
-mongoose.connect(url, {user:usr,pass:pass,useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(url, {authSource:source,user:usr,pass:pass,useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => console.log('MongoDB Connected...'))
     .catch((err) => console.log(err))
 module.exports = mongoose;
