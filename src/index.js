a43d8244491fcba739fb7431e73adaa50eaec1f8
yangoff <yangovip@gmail.com>
2021-01-07 10:18:43 -0300

    Config Umbler

--- src/index.js
+++ src/index.js
@@ -16,12 +16,12 @@ server.use('/task',TaskRoutes);
 // server.listen(3333, ()=>{
 server.listen(3000, ()=>{
     console.log(`Listen in ${Port}`);
-    figlet('API ONLINE',{font:'kban'}, function(err, data) {
-        if (err) {
-            console.log('Something went wrong...');
-            console.dir(err);
-            return;
-        }
-        console.log(data)
-    });
+    // figlet('API ONLINE',{font:'kban'}, function(err, data) {
+    //     if (err) {
+    //         console.log('Something went wrong...');
+    //         console.dir(err);
+    //         return;
+    //     }
+    //     console.log(data)
+    // });
 })
